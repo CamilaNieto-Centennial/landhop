144014dbcd2d8dd2fef6b0ea890480b220a312ed
CamilaNieto-Centennial <fourcamilitanieto@gmail.com>
2022-12-23 16:32:12 -0500

    Login, Register, and Index  Pages created

--- finalProject/settings.py
+++ finalProject/settings.py
@@ -81,6 +81,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'landhop.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
--- landhop/views.py
+++ landhop/views.py
@@ -1,3 +1,71 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.shortcuts import HttpResponse, HttpResponseRedirect, render
 from django.shortcuts import render
+from django.urls import reverse
+
+from .models import User
 
 # Create your views here.
+
+
+def index(request):
+    # Authenticated users view their index
+    if request.user.is_authenticated:
+        return render(request, "landhop/index.html")
+
+    # Everyone else is prompted to sign in
+    else:
+        return HttpResponseRedirect(reverse("login"))
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "landhop/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "landhop/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "landhop/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError as e:
+            print(e)
+            return render(request, "landhop/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "landhop/register.html")
