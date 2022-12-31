f5474fa1a6616acf44826a6b853361157da2e75e
CamilaNieto-Centennial <fourcamilitanieto@gmail.com>
2022-12-31 04:06:41 -0500

    settings.py updated

--- finalProject/settings.py
+++ finalProject/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+#DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # define global static
 #STATICFILES_DIRS = (
