859109f6716c63f4852b2fb7a92fb6e94e11dcc6
CamilaNieto-Centennial <fourcamilitanieto@gmail.com>
2022-12-31 04:25:33 -0500

    settings.py updated

--- finalProject/settings.py
+++ finalProject/settings.py
@@ -132,6 +132,4 @@ STATIC_URL = '/static/'
 #DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # define global static
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
