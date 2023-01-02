06f6c3b121e8ec1516666117cb98b581c99764fd
CamilaNieto-Centennial <fourcamilitanieto@gmail.com>
2023-01-02 09:00:23 -0500

    settings.py updated

--- finalProject/settings.py
+++ finalProject/settings.py
@@ -120,12 +120,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), '/finalProject/landhop/static/']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+
+# Added manually 👇🏻
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'landhop/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
