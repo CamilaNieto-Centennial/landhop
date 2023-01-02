86c362e9e3f53b863208629670dd4d811706924e
CamilaNieto-Centennial <fourcamilitanieto@gmail.com>
2023-01-02 09:33:02 -0500

    CSRF_TRUSTED_ORIGINS added

--- finalProject/settings.py
+++ finalProject/settings.py
@@ -135,6 +135,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://landhop.up.railway.app'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
